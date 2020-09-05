c13693351bb6449dd025e36a0b9505439403d5d1
semmelsamu <semmikroiss@gmail.com>
2020-09-05 17:35:00 +0200

    re-organized file structure

--- src/semmelsamu/php-router/examples/index.php
+++ src/semmelsamu/php-router/examples/index.php
@@ -2,9 +2,9 @@
 
 // As this is an example, we need to overwrite the Base Path. This is not nessecary if used correctly.
 error_reporting(0);
-define("BASE_PATH", "/php-router/examples");
+define("BASE_PATH", "/php-router/src/semmelsamu/php-router/examples");
 
-include("../src/php-router/main.php");
+include("../main.php");
 include("example_routes.php");
 
 $router = new Php_router($example_routes);

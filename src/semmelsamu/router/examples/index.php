d855c46743b00bfbada15daa48219bffe8a70bca
semmelsamu <semmikroiss@gmail.com>
2020-09-09 20:28:08 +0200

    finished route function

--- src/semmelsamu/router/examples/index.php
+++ src/semmelsamu/router/examples/index.php
@@ -2,8 +2,21 @@
 
     include("../index.php");
 
-    $routes = new Route();
+    $routes = new Route([
+        "file" => "index.php",
+        "routes" => [
+            "start" => new Route([
+                "file" => "index.php", 
+                "args" => true,
+                "routes" => [
+                    "site" => new Route(["file" => "site.php"]),
+                ]
+            ])
+        ]
+    ]);
 
     var_dump($routes->route());
+    echo "<br>";
+    var_dump($args);
 
 ?>
--- src/semmelsamu/router/index.php
+++ src/semmelsamu/router/index.php
@@ -2,8 +2,20 @@
 
     class Route {
 
-        function __construct() {
-            
+        function __construct($params = []) {
+            $default_params = [
+                "file" => null,
+                "id" => null,
+                "args" => false,
+                "routes" => [],
+            ];
+
+            $params = array_replace($default_params, $params);
+
+            $this->file = $params["file"];
+            $this->id = $params["id"];
+            $this->args = $params["args"];
+            $this->routes = $params["routes"];
         }
 
         function get_request() {
@@ -34,7 +46,26 @@
                 $request = $this->get_request();
             }
 
-            return false;
+            global $args;
+            $args = [];
+
+            if(sizeof($request) > 0) {
+                if(sizeof($this->routes) > 0 && array_key_exists($request[0], $this->routes)) {
+                    $current_request = array_shift($request);
+                    return $this->routes[$current_request]->route($request);
+                }
+                else {
+                    if($this->args) {
+                        $args = $request;
+                        return $this->file;
+                    }
+                }
+            }
+            else {
+                return $this->file;
+            }
+
+            return null;
         }
     }
 

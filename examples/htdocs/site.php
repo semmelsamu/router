b0a064e4a4283e4015d63d220e09fe8d00cd7889
semmelsamu <semmikroiss@gmail.com>
2021-01-10 16:24:57 +0100

    added image scale example

--- examples/htdocs/site.php
+++ examples/htdocs/site.php
@@ -1,1 +1,9 @@
-Site
+<h1>Site</h1>
+
+<?php global $router; ?>
+
+<img src="<?= $router->route_rel("img") ?>pexels-zhaocan-li-1755243.jpg?s=400">
+<p>Image with size 400 (at least 400 pixels in every dimension) ?s=400</p>
+<br>
+<img src="<?= $router->route_rel("img") ?>pexels-zhaocan-li-1755243.jpg?h=200">
+<p>Same image with height 200 ?h=200</p>

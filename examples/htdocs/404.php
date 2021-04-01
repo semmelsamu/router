3b3bc83af03b37c0a9b36378594a8fb179d27144
semmelsamu <semmikroiss@gmail.com>
2021-04-01 13:19:41 +0200

    added id(start) to 404 page

--- examples/htdocs/404.php
+++ examples/htdocs/404.php
@@ -1,2 +1,2 @@
 <h1>404 - Not found</h1>
-<p>The page you want to access doesn't exist. <a href="">Home</a></p>
+<p>The page you want to access doesn't exist. <a href="<?= $router->id("start") ?>">Home</a></p>

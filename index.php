259d0bec9bd824d229c737d17e2a0bc5c8ca578b
semmelsamu <semmikroiss@gmail.com>
2021-04-05 22:09:45 +0200

    moved index to root directory

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+// debugging
+// include("src/php/db.php");
+
+// add-on
+include("src/php/jpegscaled.php");
+
+// required
+include("src/php/get_mime_type.php");
+include("src/php/Route.php");
+include("src/php/Router.php");
+
+?>
